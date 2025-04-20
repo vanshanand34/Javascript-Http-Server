f7210b2b8280c25e364ff2c442c4e3a2050fbb29
Vansh Anand <anandvansh34@gmail.com>
2025-04-20 18:26:17 +0530

    feat: returned responses as per URL path

--- app/main.js
+++ app/main.js
@@ -5,10 +5,19 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.close();
     socket.on("close", () => {
         socket.end();
+    });
+    socket.on("data", (data) => {
+        const requestData = data.toString();
+        const requestHeaders = requestData.split("\r\n");
+        const requestPath =requestHeaders[0].split(" ")[1];
+        if (requestPath == "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        socket.end()
     });
 });
 
