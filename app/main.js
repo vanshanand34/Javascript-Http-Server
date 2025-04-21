2f21e0f1ce802fd3d49c0119ea5e6faba7e76ab9
Vansh Anand <anandvansh34@gmail.com>
2025-04-21 12:11:02 +0530

    feat: Added feature to handle post request for creating files

--- app/main.js
+++ app/main.js
@@ -4,14 +4,6 @@ const fs = require("fs");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-let directory;
-let files;
-if (process.argv.length < 4) {
-    directory = "";
-} else {
-    directory = process.argv[3];
-    files = fs.readdirSync(directory);
-}
 
 function getContentWithLength(stringToReturn, contentType = "text/plain") {
     const response = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stringToReturn.length}\r\n\r\n${stringToReturn}`
@@ -30,50 +22,69 @@ const server = net.createServer((socket) => {
         socket.end();
     });
     socket.on("data", (data) => {
-        const requestHeaders = data.toString().split("\r\n");
-        const requestPath = requestHeaders[0].split(" ")[1];
+        try {
+            const request = data.toString();
+            const requestData = request.split("\r\n");
+            const requestPath = requestData[0].split(" ")[1];
 
-        if (requestPath == "/") {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-            socket.end();
-            return;
-        }
-        else if (requestPath.includes("/echo/")) {
-            const stringToReturn = requestPath.split("/echo/")[1];
-            socket.write(getContentWithLength(stringToReturn));
-            socket.end()
-            return;
-        }
-        else if (requestPath.includes("user-agent")) {
-            let userAgent = "";
-            for (const header of requestHeaders) {
-                if (header.includes("User-Agent")) {
-                    userAgent = header.split("User-Agent: ")[1];
-                    break;
-                }
-            }
-            socket.write(getContentWithLength(userAgent));
-            socket.end();
-        }
-        else if (requestPath.includes("/files/") && directory !== "") {
-            const fileRequested = requestPath.split("/files/")[1];
-            let fileFound = "";
-            for (const file of files) {
-                if (fileRequested == file.split(".")[0]) {
-                    fileFound = file;
-                    break;
-                }
-            }
-            if (fileFound){
-                const fileContent = getFileContent(`${directory}/${fileFound}`);
-                socket.write(getContentWithLength(fileContent, "application/octet-stream"));
+            if (requestPath == "/") {
+                socket.write("HTTP/1.1 200 OK\r\n\r\n");
                 socket.end();
                 return;
             }
-        }
+            else if (requestPath.includes("/echo/")) {
+                const stringToReturn = requestPath.split("/echo/")[1];
+                socket.write(getContentWithLength(stringToReturn));
+                socket.end()
+                return;
+            }
+            else if (requestPath.includes("user-agent")) {
+                let userAgent = "";
+                for (const header of requestData) {
+                    if (header.includes("User-Agent")) {
+                        userAgent = header.split("User-Agent: ")[1];
+                        break;
+                    }
+                }
+                socket.write(getContentWithLength(userAgent));
+                socket.end();
+                return;
+            }
+            else if (requestPath.startsWith("/files/")) {
+                const directory = process.argv[3];
+                const files = fs.readdirSync(directory);
+                const fileRequested = requestPath.split("/files/")[1];
+                if (request.split(" ")[0] == "GET") {
+                    let fileFound = "";
+                    for (const file of files) {
+                        if (fileRequested == file.split(".")[0]) {
+                            fileFound = file;
+                            break;
+                        }
+                    }
+                    if (fileFound) {
+                        const fileContent = getFileContent(`${directory}/${fileFound}`);
+                        socket.write(getContentWithLength(fileContent, "application/octet-stream"));
+                        socket.end();
+                        return;
+                    }
+                } else if (request.split(" ")[0] == "POST") {
+                    const requestBody = requestData[requestData.length - 1];
+                    const file = fs.writeFileSync(`${directory}/${fileRequested}.txt`, requestBody);
+                    socket.write("HTTP/1.1 201 Created\r\n\r\n");
+                    socket.end();
+                    return;
+                }
+            }
 
-        socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
-        socket.end();
+            socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+            socket.end();
+
+        } catch (err) {
+            console.log(err);
+            socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+            socket.end();
+        }
     });
 });
 
