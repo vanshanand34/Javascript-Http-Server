aadf22f90920dee559373328920c53c3173b72e2
Vansh Anand <anandvansh34@gmail.com>
2025-04-21 10:10:52 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+console.log("arguments: ", process.argv);
 
 function getContentWithLength(stringToReturn, contentType = "text/plain") {
     const response = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stringToReturn.length}\r\n\r\n${stringToReturn}`
@@ -16,11 +17,12 @@ function getFileContent(path) {
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+    console.log(process.argv);
     socket.on("close", () => {
         socket.end();
     });
     socket.on("data", (data) => {
-
+        console.log("arguments: ", process.argv);
         const requestHeaders = data.toString().split("\r\n");
         const requestPath = requestHeaders[0].split(" ")[1];
 
@@ -46,7 +48,8 @@ const server = net.createServer((socket) => {
             socket.write(getContentWithLength(userAgent));
             socket.end();
         }
-        else if (requestPath == "/files/foo" || requestPath == "/files/foo/") {
+        else if (requestPath == "/files") {
+            console.log("directory", process.argv);
             const fileContent = getFileContent("./files/foo.txt");
             socket.write(getContentWithLength(fileContent, "application/octet-stream"));
             socket.end();
