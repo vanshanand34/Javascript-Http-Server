b15a5fc4088a2d0df2365f087c829e241d732f58
Vansh Anand <anandvansh34@gmail.com>
2025-05-01 12:33:40 +0530

    feat: Added check for compression header presence

--- app/main.js
+++ app/main.js
@@ -5,8 +5,9 @@ const fs = require("fs");
 console.log("Logs from your program will appear here!");
 
 
-function getContentWithLength(stringToReturn, closeConnectionHeader = "", contentType = "text/plain") {
-    const response = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stringToReturn.length}${closeConnectionHeader}\r\n\r\n${stringToReturn}`
+function getContentWithLength(stringToReturn, customHeaders = "", contentType = "text/plain") {
+    const response = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stringToReturn.length}${customHeaders}\r\n\r\n${stringToReturn}`
+    console.log("response\n", response)
     return response;
 }
 
@@ -26,21 +27,25 @@ const server = net.createServer((socket) => {
         const requestData = request.split("\r\n");
         const requestPath = requestData[0].split(" ")[1];
         let closeConnectionHeader = "";
+        let encodingHeader = "";
 
+        console.log(requestData);
         for (const header of requestData) {
             if (header.includes("Connection: close")) {
                 closeConnectionHeader = "\r\nConnection: close";
             }
+            if (header.endsWith("Accept-Encoding: gzip")) {
+                encodingHeader = "\r\nContent-Encoding: gzip";
+            }
         }
-        // console.log(requestData, closeConnectionHeader);
 
         try {
             if (requestPath == "/") {
-                socket.write(`HTTP/1.1 200 OK${closeConnectionHeader}\r\n\r\n`);
+                socket.write(`HTTP/1.1 200 OK${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
             }
             else if (requestPath.includes("/echo/")) {
                 const stringToReturn = requestPath.split("/echo/")[1];
-                socket.write(getContentWithLength(stringToReturn, closeConnectionHeader));
+                socket.write(getContentWithLength(stringToReturn, closeConnectionHeader + encodingHeader));
             }
             else if (requestPath.includes("user-agent")) {
                 let userAgent = "";
@@ -50,7 +55,8 @@ const server = net.createServer((socket) => {
                         break;
                     }
                 }
-                socket.write(getContentWithLength(userAgent, closeConnectionHeader));
+                console.log(userAgent);
+                socket.write(getContentWithLength(userAgent, closeConnectionHeader + encodingHeader));
             }
             else if (requestPath.startsWith("/files/")) {
                 const directory = process.argv[3];
@@ -66,20 +72,23 @@ const server = net.createServer((socket) => {
                     }
                     if (fileFound) {
                         const fileContent = getFileContent(`${directory}/${fileFound}`);
-                        socket.write(getContentWithLength(fileContent, closeConnectionHeader, "application/octet-stream"));
+                        socket.write(
+                            getContentWithLength(
+                                fileContent, closeConnectionHeader + encodingHeader, "application/octet-stream"
+                            )
+                        );
                     } else {
-                        socket.write(`HTTP/1.1 404 Not Found${closeConnectionHeader}\r\n\r\n`);
+                        socket.write(`HTTP/1.1 404 Not Found${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
                     }
                 } else if (request.split(" ")[0] == "POST") {
                     const requestBody = requestData[requestData.length - 1];
                     fs.writeFileSync(`${directory}/${fileRequested}`, requestBody);
-                    socket.write(`HTTP/1.1 201 Created${closeConnectionHeader}\r\n\r\n`);
+                    socket.write(`HTTP/1.1 201 Created${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
                 }
             } else {
-                socket.write(`HTTP/1.1 404 Not Found${closeConnectionHeader}\r\n\r\n`);
+                socket.write(`HTTP/1.1 404 Not Found${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
             }
 
-            console.log(closeConnectionHeader);
             if (closeConnectionHeader != "") {
                 socket.end();
                 socket.destroy();
