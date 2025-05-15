d76cc5bcd9d406af25f5d3f2d433114c78fb6f23
Vansh Anand <anandvansh34@gmail.com>
2025-05-15 20:53:56 +0530

    fix: Refactored the code and made some enhancements

--- app/main.js
+++ app/main.js
@@ -1,22 +1,36 @@
 const net = require("net");
 const fs = require("fs");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 
 function getContentWithLength(stringToReturn, customHeaders = "", contentType = "text/plain") {
-    const response = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stringToReturn.length}${customHeaders}\r\n\r\n${stringToReturn}`
-    console.log("response\n", response)
+    const response = (
+        `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\n` +
+        `Content-Length: ${stringToReturn.length}` +
+        `${customHeaders}\r\n\r\n` + `${stringToReturn}`
+    );
     return response;
 }
 
-function getFileContent(path) {
-    const file = fs.readFileSync(path, 'utf-8');
-    return file;
+function getFileContent(directory, files, fileRequested) {
+    try {
+        let fileFound = null;
+        for (const file of files) {
+            if (file.split('.')[0] == fileRequested) {
+                fileFound = file;
+                break;
+            }
+        }
+        if (!fileFound) return null;
+        const path = `${directory}/${fileFound}`;
+        const file = fs.readFileSync(path, 'utf-8');
+        return file;
+    } catch (err) {
+        console.log("Error while reading file : " + err);
+        return null;
+    }
 }
 
-// Uncomment this to pass the first stage
+
 const server = net.createServer((socket) => {
     console.log(process.argv);
     socket.on("close", () => {
@@ -25,68 +39,63 @@ const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const request = data.toString();
         const requestData = request.split("\r\n");
-        const requestPath = requestData[0].split(" ")[1];
-        let closeConnectionHeader = "";
-        let encodingHeader = "";
 
-        console.log(requestData);
-        for (const header of requestData) {
-            if (header.includes("Connection: close")) {
-                closeConnectionHeader = "\r\nConnection: close";
-            }
-            if (header.endsWith("Accept-Encoding: gzip")) {
-                encodingHeader = "\r\nContent-Encoding: gzip";
-            }
+        // First parsing the request line (GET /path/ HTTP/1.1)
+        const requestLine = requestData[0];
+        const requestPath = requestLine.split(" ")[1];
+        const requestMethod = requestLine.split(" ")[0];
+
+        // Parsing the request headers untill empty line indicating end of headers and start of body
+        const headers = {};
+        for (const requestPart of requestData.slice(1)) {
+            if (requestPart == '') break;
+            const parsedHeader = requestPart.split(": ");
+            headers[parsedHeader[0]] = parsedHeader.slice(1).join(": ");
         }
+
+        // Parsing request body
+        const requestBody = requestData[requestData.length - 1];
+
+        // Building response headers (if any)
+        let closeConnectionHeader = headers["Connection"] == "close" ? "\r\nConnection: close" : "";
+        let encodingHeader = headers['Accept-Encoding'] == 'gzip' ? "\r\nContent-Encoding: gzip" : "";
+        let responseHeaders = closeConnectionHeader + encodingHeader;
+
+        console.log(requestMethod, requestPath, headers);
 
         try {
             if (requestPath == "/") {
-                socket.write(`HTTP/1.1 200 OK${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
+                socket.write(`HTTP/1.1 200 OK${responseHeaders}\r\n\r\n`);
             }
             else if (requestPath.includes("/echo/")) {
                 const stringToReturn = requestPath.split("/echo/")[1];
-                socket.write(getContentWithLength(stringToReturn, closeConnectionHeader + encodingHeader));
+                socket.write(getContentWithLength(stringToReturn, responseHeaders));
             }
             else if (requestPath.includes("user-agent")) {
-                let userAgent = "";
-                for (const header of requestData) {
-                    if (header.includes("User-Agent")) {
-                        userAgent = header.split("User-Agent: ")[1];
-                        break;
-                    }
-                }
-                console.log(userAgent);
-                socket.write(getContentWithLength(userAgent, closeConnectionHeader + encodingHeader));
+                let userAgent = headers['User-Agent'] || "";
+                socket.write(getContentWithLength(userAgent, responseHeaders));
             }
             else if (requestPath.startsWith("/files/")) {
                 const directory = process.argv[3];
                 const files = fs.readdirSync(directory);
                 const fileRequested = requestPath.split("/files/")[1];
-                if (request.split(" ")[0] == "GET") {
-                    let fileFound = "";
-                    for (const file of files) {
-                        if (fileRequested == file.split(".")[0]) {
-                            fileFound = file;
-                            break;
-                        }
-                    }
-                    if (fileFound) {
-                        const fileContent = getFileContent(`${directory}/${fileFound}`);
+                if (requestMethod == "GET") {
+                    const fileContent = getFileContent(directory, files, fileRequested);
+                    if (fileContent) {
                         socket.write(
                             getContentWithLength(
-                                fileContent, closeConnectionHeader + encodingHeader, "application/octet-stream"
+                                fileContent, responseHeaders, contentType = "application/octet-stream"
                             )
                         );
                     } else {
-                        socket.write(`HTTP/1.1 404 Not Found${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
+                        socket.write(`HTTP/1.1 404 Not Found${responseHeaders}\r\n\r\n`);
                     }
-                } else if (request.split(" ")[0] == "POST") {
-                    const requestBody = requestData[requestData.length - 1];
+                } else if (requestMethod == "POST") {
                     fs.writeFileSync(`${directory}/${fileRequested}`, requestBody);
-                    socket.write(`HTTP/1.1 201 Created${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
+                    socket.write(`HTTP/1.1 201 Created${responseHeaders}\r\n\r\n`);
                 }
             } else {
-                socket.write(`HTTP/1.1 404 Not Found${closeConnectionHeader}${encodingHeader}\r\n\r\n`);
+                socket.write(`HTTP/1.1 404 Not Found${responseHeaders}\r\n\r\n`);
             }
 
             if (closeConnectionHeader != "") {
